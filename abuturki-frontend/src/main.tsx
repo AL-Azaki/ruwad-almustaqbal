aad06a18c8325cf06752a4a9bfdbb2681ff3f452
Basheer <bashiralazaki@gmail.com>
2026-06-13 22:54:02 +0300

    Add dynamic Google Analytics initialization via Environment Variables

--- abuturki-frontend/src/main.tsx
+++ abuturki-frontend/src/main.tsx
@@ -8,6 +8,24 @@ import { SettingsProvider } from './contexts/SettingsContext'
 
 import { HelmetProvider } from 'react-helmet-async'
 
+// Initialize Google Analytics dynamically if the ID is provided in Netlify Env
+const gaId = import.meta.env.VITE_GA_ID;
+if (gaId) {
+  const script1 = document.createElement('script');
+  script1.async = true;
+  script1.src = `https://www.googletagmanager.com/gtag/js?id=${gaId}`;
+  document.head.appendChild(script1);
+
+  const script2 = document.createElement('script');
+  script2.innerHTML = `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '${gaId}');
+  `;
+  document.head.appendChild(script2);
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HelmetProvider>
